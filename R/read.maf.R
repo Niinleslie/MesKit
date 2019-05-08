228d5c5b0c8c4589434c212343bd764b272e740c
Wang Chengwei <504430784@qq.com>
2019-05-08 22:40:54 +0800

    change name

--- R/read.maf.R
+++ R/read.maf.R
@@ -9,17 +9,84 @@
 #' @examples
 #' read.maf(patientID, maf.dir = "./data/maf", SampleInfo.dir = "./data/") 
 
+library(maftools)
 
+## MAF object
+MAF <- setClass(Class = 'MAF', slots =  c(data = 'data.table', variants.per.sample = 'data.table', variant.type.summary = 'data.table',
+                                          variant.classification.summary = 'data.table', gene.summary = 'data.table',
+                                          summary = 'data.table', maf.silent = 'data.table'))
+
+setMethod(f = 'show', signature = 'MAF', definition = function(object){
+  cat(paste('An object of class ', class(object), "\n"))
+  print(object@summary)
+})
+
+
+# directories
+maf.dir = "/home/ninomoriaty/R_Project/MesKit/inst/extdata/multi_lesion/maf"
+patientID = "311252"
+SampleInfo.dir = "/home/ninomoriaty/R_Project/MesKit/inst/extdata/multi_lesion/sample_info.txt"
+
+
+# maftools comparasion
+maftools.result <- read.maf(paste(maf.dir,'/',patientID,'.maf',sep = ""))
+# vcs = getSampleSummary(maftools.result)
+maftools.result@summary
+maftools.result@variant.classification.summary
+m <- MAF(data = maftools.result@data, variants.per.sample = maftools.result@variants.per.sample, variant.type.summary = maftools.result@variant.type.summary,
+         variant.classification.summary = maftools.result@variant.classification.summary, gene.summary = maftools.result@gene.summary,
+         summary = maftools.result@summary, maf.silent = maftools.result@maf.silent)
+
+plotmafSummary(maf = m, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+
+# read.maf main function
 read.maf <- function(patientID, maf.dir, SampleInfo.dir){
-  #read maf file
+  # read maf file
   maf_input <- read.table(paste(maf.dir,'/',patientID,'.maf',sep = ""), quote = "", header = TRUE, fill = TRUE, sep = '\t')
-  #read info file
+  # read info file
   sample_info_input <-  read.table(SampleInfo.dir, quote = "", header = TRUE, fill = TRUE, sep = '')
-  #Generate patient,lesion and time information in the last three columns of the maf file
-  maf_input$patient=''
-  maf_input$lesion=''
-  maf_input$time=''
-  maf_input$mut.id=''
+  # Generate patient,lesion and time information in the last three columns of the maf file
+  maf_input$patient = ""
+  maf_input$lesion = ""
+  maf_input$time = ""
+  
+  
+  # combine sample_info_input with maf_input
+  tsb_ls_total <- unique(sample_info_input$sample)
+  tsb_ls <- tsb_ls_total[which(tsb_ls_total %in% maf_input$Tumor_Sample_Barcode)]
+  
+  for (tsb in tsb_ls) {
+    patient <- sample_info_input[which(sample_info_input$sample == tsb),]$patient
+    lesion <- as.character(sample_info_input[which(sample_info_input$sample == tsb),]$lesion)
+    time <- as.character(sample_info_input[which(sample_info_input$sample == tsb),]$time)
+    maf_input[which(maf_input$Tumor_Sample_Barcode == tsb),]$patient <- patient
+    maf_input[which(maf_input$Tumor_Sample_Barcode == tsb),]$lesion <- lesion
+    maf_input[which(maf_input$Tumor_Sample_Barcode == tsb),]$time <- time
+  }
+  
+  maf_input$Variant_Classification = as.factor(as.character(maf_input$Variant_Classification))
+  maf_input$Variant_Type = as.factor(as.character(maf_input$Variant_Type))
+  
+  nGenes = length(unique(maf_input$Hugo_Symbol))
+  maf.tsbs = levels(maf_input$Tumor_Sample_Barcode)
+  nSamples = length(levels(maf_input$Tumor_Sample_Barcode))
+  
+  maf.data <- data.table::setDT(maf_input)
+  
+  # MAF:data
+  tsb = maf_input[,.N, Tumor_Sample_Barcode]
+  colnames(tsb)[2] = 'Variants'
+  tsb = tsb[order(tsb$Variants, decreasing = TRUE),]
+  
+  # MAF:
+  
+  # mafSummary = summarizeMaf(maf = maf_input, anno = clinicalData, chatty = verbose)
+  # 
+  # m = MAF(data = maf, variants.per.sample = mafSummary$variants.per.sample, variant.type.summary = mafSummary$variant.type.summary,
+  #         variant.classification.summary = mafSummary$variant.classification.summary, gene.summary = mafSummary$gene.summary,
+  #         summary = mafSummary$summary, maf.silent = maf.silent, clinical.data = mafSummary$sample.anno)
+  
+
   h <- function(x){
     Hugo_Symbol=x[1]
     Chromosome=x[2]
