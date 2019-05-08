e2e1b9fa5ad3695f14bae87785aafd0c8ff095c2
Wang Chengwei <504430784@qq.com>
2019-05-08 22:48:37 +0800

    change all rules

--- R/read.maf.R
+++ R/read.maf.R
@@ -50,11 +50,9 @@ read.maf <- function(patientID, maf.dir, SampleInfo.dir){
   maf_input$lesion = ""
   maf_input$time = ""
   
-  
   # combine sample_info_input with maf_input
   tsb_ls_total <- unique(sample_info_input$sample)
   tsb_ls <- tsb_ls_total[which(tsb_ls_total %in% maf_input$Tumor_Sample_Barcode)]
-  
   for (tsb in tsb_ls) {
     patient <- sample_info_input[which(sample_info_input$sample == tsb),]$patient
     lesion <- as.character(sample_info_input[which(sample_info_input$sample == tsb),]$lesion)
@@ -64,47 +62,8 @@ read.maf <- function(patientID, maf.dir, SampleInfo.dir){
     maf_input[which(maf_input$Tumor_Sample_Barcode == tsb),]$time <- time
   }
   
-  maf_input$Variant_Classification = as.factor(as.character(maf_input$Variant_Classification))
-  maf_input$Variant_Type = as.factor(as.character(maf_input$Variant_Type))
-  
-  nGenes = length(unique(maf_input$Hugo_Symbol))
-  maf.tsbs = levels(maf_input$Tumor_Sample_Barcode)
-  nSamples = length(levels(maf_input$Tumor_Sample_Barcode))
-  
-  maf.data <- data.table::setDT(maf_input)
-  
-  # MAF:data
-  tsb = maf_input[,.N, Tumor_Sample_Barcode]
-  colnames(tsb)[2] = 'Variants'
-  tsb = tsb[order(tsb$Variants, decreasing = TRUE),]
-  
-  # MAF:
-  
-  # mafSummary = summarizeMaf(maf = maf_input, anno = clinicalData, chatty = verbose)
   # 
-  # m = MAF(data = maf, variants.per.sample = mafSummary$variants.per.sample, variant.type.summary = mafSummary$variant.type.summary,
-  #         variant.classification.summary = mafSummary$variant.classification.summary, gene.summary = mafSummary$gene.summary,
-  #         summary = mafSummary$summary, maf.silent = maf.silent, clinical.data = mafSummary$sample.anno)
   
-
-  h <- function(x){
-    Hugo_Symbol=x[1]
-    Chromosome=x[2]
-    Start_Position=x[3]
-    End_Position=x[4]
-    Tumor_Seq_Allele1=x[8]
-    Tumor_Seq_Allele2=x[9]
-    mut.id=paste(Hugo_Symbol,Chromosome,Start_Position,End_Position,Tumor_Seq_Allele1,Tumor_Seq_Allele2,sep = '_')
-    x[19]=mut.id
-    #Find the location of sample at sample_info
-    row <- which(x[15]==sample_info_input$sample)
-    x[16]=as.character(sample_info_input$patient[row])
-    x[17]=as.character(sample_info_input$lesion[row])
-    x[18]=as.character(sample_info_input$time[row])
-    return(x)
-  }
-  maf_input=t(apply(maf_input,1,h))
-  return(maf_input)
 }
 
 
