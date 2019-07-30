2fe094555fef85fbfd7f838783ba3690d18bcc7c
chenjy327 <364500513@qq.com>
2019-07-30 14:56:10 +0800

    update shiny app

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,6 +1,215 @@
 suppressMessages(library(shiny))
 
+
+
 # Define server logic required to plot various variables against mpg
-shinyServer(function(input, output) {
+shinyServer(function(input, output){
   
+  phylotree.type <- reactive({
+    return(input$phylotTreeType)
+  })
+  width1 <- reactive({
+    return(input$width1)
+  })
+  height1 <- reactive({
+    return(input$height1)
+  })
+  width2 <- reactive({
+    return(input$width2)
+  })
+  height2 <- reactive({
+    return(input$height2)
+  })
+  
+  maf_file <- reactive({input$maf})
+  sample_info_file <- reactive({input$sampleInfo})
+  maf_input <-  reactive({read.table(maf_file()$datapath, quot = "", header = TRUE, fill = TRUE, sep = '\t')}) 
+  sample_info_input <-  reactive({read.table(sample_info_file()$datapath , quot = "", header = TRUE, fill = TRUE, sep = '')}) 
+  patientID <-  reactive({strsplit(maf_file()$name,'.maf')[[1]]}) 
+  ccf.cluster.tsv_file <- reactive({input$ccf.cluster}) 
+  ccf.loci.tsv_file <- reactive({input$ccf.loci})
+  ccf.cluster.tsv_input <-  reactive({read.table(ccf.cluster.tsv_file()$datapath, quot = "", header = TRUE, fill = TRUE, sep = '\t', stringsAsFactors=F)}) 
+  ccf.loci.tsv_input <- reactive({read.table(ccf.loci.tsv_file()$datapath, quot = "", header = TRUE, fill = TRUE, sep = '\t', stringsAsFactors=F)})
+  phylotree.dir <- reactive({
+    infile <- input$phylotree.dir
+    phylotree.dir <- input
+  })
+
+  upload_maf <- eventReactive(input$submit1,{
+    read.Maf.Shiny(maf_input =  maf_input(), 
+             sample_info_input =  sample_info_input(), use.ccf = input$useccf,
+            ccf.cluster.tsv_input = ccf.cluster.tsv_input(),
+            ccf.loci.tsv_input = ccf.loci.tsv_input(),ref.build = input$ref)
+    })
+  upload_njtree <- eventReactive(input$submit3,{
+    maf <- upload_maf()
+    njtree <- NJtree(maf,use.indel = input$use.indel,use.ccf = input$useccf )
+  })
+  
+  output$mafSummary <- renderPlot({
+    maf <- upload_maf()
+    plotmafSummary(maf = maf)
+  },
+  width = width1,
+  height = height1
+  )
+  output$mathScore <- renderDataTable({
+    maf <- upload_maf()
+    MATH_score(maf)
+  })
+  output$vaf.cluster <- renderPlot({
+    maf <- upload_maf()
+    VAF_plot(maf)}, 
+    width = width1,
+    height = height1
+  )
+  output$mut.share_private <- renderPlot({
+    maf <- upload_maf()
+    mutSharedPrivate(maf)
+  },
+  width = width1,
+  height = height1
+  )
+  output$cloneplot <- renderPlot({
+    validate(
+      need(!(is.null(input$ccf.cluster1$datapath)), "Upload ccf.cluster")
+    )
+    validate(
+      need(!(is.null(input$ccf.loci1$datapath)), "Upload ccf.loci ")
+    )
+    validate(
+      need(input$okk != 0, "Press"))
+    TumorClones_plot.shiny(patientID = input$patientid,
+                           ccf.cluster.dir = input$ccf.cluster1$datapath,
+                           ccf.loci.dir = input$ccf.loci1$datapath,
+                           out.dir = '.')
+  })
+  
+  output$GOplot <- renderPlot({
+    njtree <- upload_njtree()
+    GO.njtree.shiny(njtree, savePlot = F, qval = input$qval ,pval = input$pval)
+  },
+  width = width2,
+  height = height2
+  )
+  output$Pathwayplot <- renderPlot({
+    njtree <- upload_njtree()
+    Pathway.njtree.shiny(njtree, savePlot = F, qval = input$qval ,pval = input$pval)
+  },
+  width = width2,
+  height = height2
+  )
+  
+  output$phylotree <- renderPlot({
+    if(input$phyloTreeType == 'njtree'){
+      maf <- upload_maf()
+      validate(
+        need(!(input$submit.tree),"Press tht button if you choose parameter")
+      )
+      plot.PhyloTree(maf,use.indel = input$use.indel,heatmap.type = input$heatmap.type ,show.mutSig = input$show.mutSig,
+                show.heatmap = input$show.heatmap, output.dir = '',phylotree.type = input$phyloTreeType)
+    }
+    else{
+      validate(
+        need(!is.null(input$phylotree.dir),"Upload your phylotree file")
+      )
+      validate(
+        need((input$submit.tree),"press button") 
+      )
+      plot.PhyloTree(phylotree.dir = input$phylotree.dir$datapath,phylotree.type = input$phyloTreeType,output.dir = '')
+    }
+    
+  })
+  
+  output$DownloadMafSummary <- downloadHandler(
+    
+    filename = function() {
+      paste("MafSummary", Sys.time(), '.',input$DownloadMafSummaryCheck, sep='')
+    },
+    content = function(file) {
+
+      if (input$DownloadMafSummaryCheck == "png"){
+        png(file,width = 14 , height = 8)
+      }
+      else if (input$DownloadMafSummaryCheck == "pdf"){
+        pdf(file,width = 14 , height = 8)
+      }
+      maf <- upload_maf()
+      plotmafSummary(maf = maf)
+      dev.off()
+    },
+    contentType = paste('image/',input$DownloadMafSummaryPlotCheck,sep="")
+  )
+  
+  output$DownloadVafPlot <- downloadHandler(
+    filename = function() {
+      paste("VafPlot", Sys.time(), '.',input$DownloadVafPlotCheck, sep='')
+    },
+    content = function(file) {
+      if (input$DownloadVafPlotCheck == "png"){
+        png(file,width = 14 , height = 8)
+      }
+      else if (input$DownloadVafPlotCheck == "pdf"){
+        pdf(file,width = 14 , height = 8)
+      }
+      maf <- upload_maf()
+      VAF_plot(maf = maf)
+      dev.off()
+    },
+    contentType = paste('image/',input$DownloadVafPlotCheck,sep="")
+  )
+  
+  output$DownloadSharedPlot <- downloadHandler(
+    filename = function() {
+      paste("SharedPlot", Sys.time(), '.',input$DownloadSharedPlotCheck, sep='')
+    },
+    content = function(file) {
+      if (input$DownloadSharedPlotCheck == "png"){
+        png(file,width = 14 , height = 8)
+      }
+      else if (input$DownloadSharedPlotCheck == "pdf"){
+        pdf(file,width = 14 , height = 8)
+      }
+      maf <- upload_maf()
+      mutSharedPrivate(maf = maf)
+      dev.off()
+    },
+    contentType = paste('image/',input$DownloadSharedPlotCheck,sep="")
+  )
+  output$DownloadClonePlot <- downloadHandler(
+    filename = function() {
+      paste("ClonePlot", Sys.time(), '.',input$DownloadClonePlotCheck, sep='')
+    },
+    content = function(file) {
+      if (input$DownloadClonePlotCheck == "png"){
+        png(file,width = 14 , height = 8)
+      }
+      else if (input$DownloadClonePlotCheck == "pdf"){
+        pdf(file,width = 14 , height = 8)
+      }
+      maf <- upload_maf()
+      TumorClones_plot(maf = maf)
+      dev.off()
+    },
+    contentType = paste('image/',input$DownloadClonePlotCheck,sep="")
+  )
+output$DownloadPhyloTree <- downloadHandler(
+  filename = function() {
+    paste("PhyloTree", Sys.time(), '.',input$DownloadPhyloTreeCheck, sep='')
+  },
+  content = function(file) {
+    if (input$DownloadPhyloTreeCheck == "png"){
+      png(file,width = 14 , height = 8)
+    }
+    else if (input$DownloadPhyloTreeCheck == "pdf"){
+      pdf(file,width = 14 , height = 8)
+    }
+    maf <- upload_maf()
+    plot.PhyloTree(maf = maf)
+    dev.off()
+  },
+  contentType = paste('image/',input$DownloadPhyloTreeCheck,sep="")
+)
 })
+
+
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -9,16 +9,16 @@ suppressMessages(library(shinycssloaders))
 #sider bar----
 
 sidebar <- dashboardSidebar(
-  width = 300,
+    width = 300,
   sidebarMenu(id="sidername",selected='home',
     menuItem("Home", tabName = "home", icon = icon("home")),
-    menuItem("MesKit overview", tabName = "MesKit", icon = icon("eye")),
     menuItem("ITH evaluation", tabName = "ITH", icon = icon("location-arrow")),
     menuItem("Clonal analysis", tabName = "clone", icon = icon("th",lib = "glyphicon")),
     menuItem("Functional analysis", tabName = "function", icon = icon("bar-chart")),
-    menuItem("Other analysis", tabName = "Survival", icon = icon("line-chart"))
+    menuItem("PhyloTree", tabName = "Survival", icon = icon("line-chart"))
   )
 )
+
 
 #bodyHome ----
 #tabItem:创建标签页；子页面；与sidebar里的tabName相对应
@@ -61,6 +61,197 @@ bodyHome <- tabItem("home",
                 )
 )
 
+
+bodyITH <- tabItem("ITH",
+                   h2('ITH evaluation'),
+                   fluidRow(
+                     box(
+                       title = strong('Upload/Setting'),
+                       status = 'primary',
+                       solidHeader = T,
+                       width = 3,
+                       id = 'upload',
+                       fileInput('maf','Upload your maf file'),
+                       fileInput('sampleInfo','Upload sampleInfo' ),
+                       textInput('patientid','Input patient ID'),
+                       selectInput('ref','select reference genome',
+                                   choices = c(
+                                     'hg19','hg38'
+                                   )),
+                       checkboxInput('useccf','choose if  use ccf',value = F),
+                       conditionalPanel(
+                         condition = "input.useccf == true",
+                         fileInput('ccf.cluster','Upload ccf.cluster'),
+                         fileInput('ccf.loci','Upload ccf.loci' )
+                       ),
+                       sliderInput('width1','adjust image width',min = 700,max = 1100, value = 850),
+                       sliderInput('height1','adjust image height',min = 400,max = 570, value = 560),
+                       actionBttn('submit1',div(
+                         strong("Click ME to start analysing"),align = 'center',
+                         icon("hand-right", lib = "glyphicon")))
+                     ),
+                     box(
+                       h3(strong('Visuliaztion')),
+                       width = 9,
+                       height = 900,
+                       tabBox(                           
+                         height = "100%", width = "100%",
+                         selected = "caInput01",
+                         side = "left",
+                         tabPanel(
+                           title = div(icon("table"), "MafSummary"),
+                           value = "caInput01",
+                           withSpinner(plotOutput('mafSummary',height = '100%')),
+                           radioButtons('DownloadMafSummaryCheck','Choose file type to download:',
+                                        c('png' ='png','pdf' = 'pdf'),inline = T),
+                           downloadBttn('DownloadMafSummary', 'Download')
+                         ),
+                         tabPanel(
+                           title = div(icon("table"), "Math.Score"),
+                           value = "caInput02",
+                           withSpinner(dataTableOutput('mathScore'))                 
+                         ),
+                         tabPanel(
+                           title = div(icon("table"), "VAF plot"),
+                           value = "caInput03",
+                           withSpinner(plotOutput("vaf.cluster",height = 600,width = 7)),
+                           radioButtons('DownloadVafPlotCheck','Choose file type to download:',
+                                        c('png' ='png','pdf' = 'pdf'),inline = T),
+                           downloadBttn('DownloadVafPlot', 'Download')
+                         ),
+                         tabPanel(
+                           title = div(icon("table"), "SharePrivate"),
+                           value = "caInput04",
+                           withSpinner(plotOutput("mut.share_private")),
+                           radioButtons('DownloadSharePlotCheck','Choose file type to download:',
+                                        c('png' ='png','pdf' = 'pdf'),inline = T),
+                           downloadBttn('DownloadSharePlot', 'Download')
+                         )
+                         )
+                     )
+                   )
+)
+
+bodyclone <- tabItem('clone',
+                    fluidRow(
+                      box(
+                        title = strong('Upload/Setting'),
+                        status = 'primary',
+                        solidHeader = T,
+                        width = 3 ,
+                        fileInput('ccf.cluster1','Upload ccf.cluster'),
+                        fileInput('ccf.loci1','Upload ccf.loci' ),
+                        actionBttn('okk',div(
+                          strong("Click ME to visualize result"),align = 'center',
+                          icon("hand-right", lib = "glyphicon")))
+                      ),
+                      box(
+                        h3(strong('Visuliaztion')),
+                        width = 9,
+                        tabBox(
+                          selected = 'c01',
+                          side = 'left',
+                          tabPanel(
+                            value = 'c01',
+                            title = div(icon("table"), "TumorClonePlot"),
+                            withSpinner(plotOutput('cloneplot')),
+                           radioButtons('DownloadClonePlotCheck','Choose file type to download:',
+                                     c('png' ='png','pdf' = 'pdf'),inline = T),
+                           downloadBttn('DownloadClonePlot', 'Download')
+                          )
+                        )
+                        
+                      )
+                    )
+)
+
+bodyfunction <- tabItem('function',
+                        h2(strong('Functional analysis')),
+                        fluidRow(
+                          column(
+                            width = 3,
+                            box(
+                             width = NULL,
+                             status = 'primary',
+                             h3(strong('Adjust pval/qval')),
+                             sliderInput('pval','adjust pval',min = 0,max = 1, value = 0.05),
+                             sliderInput('qval','adjust image qval',min = 0,max = 1, value = 0.2),
+                             actionBttn('submit3',div(strong("Click me Start/Refresh"),align = 'center'))
+                           ),
+                          box(
+                             width = NULL,
+                             status = 'primary',
+                             h3(strong('Adjust image height/width')),
+                             sliderInput('width2','adjust image width',min = 700,max = 1100, value = 850),
+                             sliderInput('height2','adjust image height',min = 800,max = 2000, value = 1300)
+                          )
+                          ),
+                          column(
+                           width = 9,
+                           box(
+                            width = NULL,
+                            height = 2100,
+                            tabBox(
+                              side = 'left',
+                              selected = 'F01',
+                              width = "100%",
+                              height = "100%",
+                              tabPanel(
+                                title = 'GO analysis',
+                                value = 'F01',
+                                withSpinner(plotOutput('GOplot',height = "100%",width = "100%"))
+                              ),
+                              tabPanel(
+                                title = 'pathway analysis',
+                                value = 'F02',
+                                withSpinner(plotOutput('Pathwayplot',height = "100%",width = "100%"))
+                              )
+                            )
+                          )
+                          )
+                         
+
+                        ))
+
+bodySurvival <- tabItem('Survival',
+                        h2('Phylotree Visualiaztion'),
+                        fluidRow(
+                          box(
+                            title = strong('Upload/Setting'),
+                            status = 'primary',
+                            solidHeader = T,
+                            width = 3,
+                            radioButtons('phyloTreeType','select tye of your phylotree ',
+                                         c( NJtree = 'njtree',
+                                            Newick = 'newick',
+                                            beast = 'beast',
+                                            PAML = 'PAML'),selected = 'njtree',inline = T),
+                            h3(strong("parameters")),
+                            checkboxInput('use.indel','choose if  use indel',value = FALSE),
+                            checkboxInput('show.mutSig','choose if show mutationignature',value = TRUE),
+                            checkboxInput('show.heatmap','choose if show heatmap',value = TRUE),
+                            textInput('heatmap.type','heatmap type','binary'),
+                            conditionalPanel(
+                              condition = "input.phyloTreeType != 'njtree'",
+                              fileInput('phylotree.dir','upload your phylotree file')
+                            ),
+                              actionBttn('submit.tree',div(
+                              strong("Click ME to visualize result"),align = 'center',
+                              icon("hand-right", lib = "glyphicon")))
+                          ),
+                          box(
+                            width = 9,
+                            height = 950 ,
+                            withSpinner(plotOutput("phylotree",height = 800)) ,    
+                            radioButtons('DownloadPhyloTreeCheck','Choose file type to download:',
+                                         c('png' ='png','pdf' = 'pdf'),inline = T),
+                            downloadBttn('DownloadPhyloTree', 'Download')
+                          )
+                        ),
+                        fluidRow(
+                        ))
+                    
+
 #Main function----
 shinyUI(
   dashboardPage(skin = "blue",
@@ -73,7 +264,11 @@ shinyUI(
         tags$link(rel = "stylesheet", type = "text/css", href = "css/main.css")
       ),
       tabItems(
-        bodyHome
+        bodyHome,
+        bodyITH,
+        bodyclone,
+        bodyfunction,
+        bodySurvival
       )
     )
   )
