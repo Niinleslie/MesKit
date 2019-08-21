fa4651a190655468eedc88cc3e91889bc33feb4b
chenjy327 <364500513@qq.com>
2019-08-21 21:21:09 +0800

    modify NJtree.R plot.PhyloTree.R heatmap.R and file name

--- R/NJtree.R
+++ R/NJtree.R
@@ -3,8 +3,8 @@
 #' @param maf return from readMaf()
 #' @param use.indel Seclet SNP in Variant type
 #' @param sig.min.mut.number minimum mutation number in each branch
-#' @param ccf.mutation.id determine the id of mutation
-#' @param ccf.mutation.sep sep of id
+#' @param ccf.mutation.id manually specify which columns could be joint by ccf.mutation.sep to get the same format of mutation id in ccfy
+#' @param ccf.mutation.sep manually specify the separator character.Values on each line of the ccf.mutation.id are separated by this character. I
 #' @return NJtree object
 #' 
 #' @examples
@@ -12,44 +12,37 @@
 #' sampleInfo.File <- system.file("extdata/multi_lesion", "sample_info.txt", package = "Meskit")
 #' pyCloneCluster <- system.file("extdata/multi_lesion/ccf", "311252.cluster.tsv", package = "Meskit")
 #' pyCloneLoci <- system.file("extdata/multi_lesion/ccf", "311252.loci.tsv", package = "Meskit")
-#' maf <- readMaf(patientID = "311252", mafFile = maf.File, sampleInfoFile = sampleInfo.File, refBuild = "hg19")
+#' maf <- readMaf(patientID = "311252", mafFile = maf.File, sampleInfo = sampleInfo.File, refBuild = "hg19")
 #' njtree <- NJtree(maf)
-#' getNJtreenj(njtree)
-#' getNJtreemut_sort(njtree)
-#' getNJtreeSignature(njtree)
 
 
 # set NJtree object
-NJtree <- function(maf, use.indel = FALSE, use.ccf = FALSE, mut.signature = TRUE, 
-                   sig.min.mut.number = 50, ccf.mutation.id, ccf.mutation.sep){
+NJtree <- function(maf, use.indel = FALSE, mut.signature = TRUE, 
+                   sig.min.mut.number = 50, ccf.mutation.id = c("Hugo_Symbol","Chromosome","Start_Position"),
+                   ccf.mutation.sep = ":"){
   maf.dat <- maf@data
-  ccf <- maf@ccf.loci
   patientID <- maf@patientID
-  if(use.ccf){
-    mut_sort <- mut_ccf_sort(maf.dat = maf.dat, ccf = ccf, use.indel, ccf.mutation.id, ccf.mutation.sep)
-    if(is.null(ccf)){
-      stop("Missing ccf file. Check whether maf@ccf.loci is NULL")
-    }
-  }else{
-    mut_sort <- mut_binary_sort(maf.dat = maf.dat, use.indel = use.indel)
-  }
+  refBuild <- paste("BSgenome.Hsapiens.UCSC.", maf@ref.build, sep = "")
+
+  mut_sort <- mut_binary_sort(maf.dat = maf.dat, use.indel = use.indel)
   mat.nj = nj(dist.gene(t(mut_sort)))
   branch <- read.njtree(mat.nj)
-  mut.branches <- .treeMutationalBranches(maf, branch)
-  njtree <- new('NJtree', nj = mat.nj, mut_sort = mut_sort, patientID = patientID, mut_branches = mut.branches)
+  mut.branches <- treeMutationalBranches(maf, branch)
+  ccf_sort <- matrix()
+  if(is.null(maf@ccf.loci)){
+    ccf_sort <- mut_ccf_sort(maf.dat = maf.dat, ccf = ccf, use.indel, ccf.mutation.id, ccf.mutation.sep)
+  }
+
+  njtree <- new('NJtree', patientID = patientID, nj = mat.nj, mut_sort = mut_sort, ccf_sort = ccf_sort, mut_branches = mut.branches, refBuild = maf@ref.build)
   return(njtree)
 }
 #Prevent class 'phylo' from not existing
 setClass('phylo')
 #Class NJtree
-setClass('NJtree', slots = c(nj = 'phylo', mut_sort = 'matrix', patientID = 'character', mut_branches = 'list'))
+setClass('NJtree', slots = c(nj = 'phylo', patientID = 'character', mut_sort = 'matrix', ccf_sort = 'matrix', mut_branches = 'list', refBuild = 'character'))
 #extract mut_sort from NJtree object
 setGeneric("getMutSort", function(x, signature){standardGeneric("getMutSort")})
 setMethod("getMutSort", 'NJtree', function(x){x@mut_sort})
 #extract nj from NJtree object
 setGeneric("getPhyloTree", function(x, signature){standardGeneric("getPhyloTree")})
 setMethod("getPhyloTree",'NJtree', function(x){x@nj})
-#extract siganture from NJtree object
-setGeneric("getNJtreeSignature", function(x, signature){standardGeneric("getNJtreeSignature")})
-setMethod("getNJtreeSignature", 'NJtree', function(x){x@signature})
-
