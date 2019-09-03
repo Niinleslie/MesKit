ea0d1c807508d31664cb289489a99742d8f4a9cc
NinoMoriaty <504430784@qq.com>
2019-09-03 21:35:30 +0800

    suppressMessages dcast

--- R/mut_sort.R
+++ R/mut_sort.R
@@ -28,7 +28,7 @@ mut_ccf_sort <- function(maf.dat, ccf, use.indel, ccf.mutation.id , ccf.mutation
   M <- maf_preprocess(maf.dat, use.indel = use.indel, use.ccf = TRUE, ccf, ccf.mutation.id = ccf.mutation.id, ccf.mutation.sep = ccf.mutation.sep)
   M[is.na(M$CCF),'CCF'] <- 2
 
-  mut_samples <- dcast(M, mut.id~sample, value.var = "CCF")[,-1]
+  mut_samples <- suppressMessages(dcast(M, mut.id~sample, value.var = "CCF")[,-1])
   #mut_samples[mut_samples == 0 ] <- 2
   mut_samples[is.na(mut_samples)] <- 0
   mut_samples$NORMAL <- 0
@@ -40,7 +40,7 @@ mut_ccf_sort <- function(maf.dat, ccf, use.indel, ccf.mutation.id , ccf.mutation
 ##sort the matrix; return row order and coloum order
 mut_binary_sort <- function(maf.dat, use.indel = FALSE, returnOrder = FALSE){
   M <- maf_preprocess(maf.dat, use.indel = use.indel)
-  mut_samples <- dcast(M, mut.id~sample)[,-1]
+  mut_samples <- suppressMessages(dcast(M, mut.id~sample)[,-1])
   mut_samples[!is.na(mut_samples)] <- 1
   mut_samples[is.na(mut_samples)] <- 0
   mut_samples$NORMAL <- 0
