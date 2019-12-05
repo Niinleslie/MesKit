a8b71f13fa6d3e2006d71179423422316da5bb49
Ninomoriaty <504430784@qq.com>
2019-12-05 23:58:38 +0800

    para name: logic refined

--- R/readMaf.R
+++ R/readMaf.R
@@ -34,133 +34,136 @@
 
 ## read.maf main function
 readMaf <- function(
-    ## maf parameters
-    mafFile, sampleInfoFile, ID=NULL, 
-    ## filter selection
-    mutType="All", mutNonSilent=NULL, chrSilent=NULL, use.indel=FALSE, 
-    ## ccf parameters             
-    ccfClusterTsvFile=NULL, ccfLociTsvFile=NULL, 
-    ## supplyment
-    refBuild="hg19"
-    ){
+  ## maf parameters
+  mafFile, sampleInfoFile, ID=NULL, 
+  ## filter selection
+  mutType="All", mutNonSilent=NULL, chrSilent=NULL, use.indel=FALSE, 
+  ## ccf parameters             
+  ccfClusterTsvFile=NULL, ccfLociTsvFile=NULL, 
+  ## supplyment
+  refBuild="hg19"
+){
+  
+  ## read maf file
+  if (.substrRight(mafFile, 3) == ".gz"){
+    mafInput <- read.table(mafGz <- gzfile(mafFile, "r"), quote="", 
+                           header=TRUE, fill=TRUE, 
+                           sep='\t', stringsAsFactors=FALSE)
     
-    ## read maf file
-    if (.substrRight(mafFile, 3) == ".gz"){
-        mafInput <- read.table(mafGz <- gzfile(mafFile, "r"), quote="", 
-                               header=TRUE, fill=TRUE, 
-                               sep='\t', stringsAsFactors=FALSE)
-        
-        close(mafGz)
-    } else {
-        mafInput <- read.table(mafFile, quote="", 
-                               header=TRUE, fill=TRUE, 
-                               sep='\t', stringsAsFactors=FALSE)
-    }
-    
-    ## get patientID
+    close(mafGz)
+  } else {
+    mafInput <- read.table(mafFile, quote="", 
+                           header=TRUE, fill=TRUE, 
+                           sep='\t', stringsAsFactors=FALSE)
+  }
+  
+  ## get patientID
+  if (!is.null(ID)) {
+    patientID <- ID
+  } else {
     fileName <- unlist(strsplit(mafFile, "/"))[length(unlist(strsplit(mafFile, "/")))]
     patientID <- strsplit(as.character(fileName), ".maf")[[1]][1]
     ## correct error name of fileinput on shiny app (Original para patientID could solve the problem)
     if(patientID == 0){
       patientID <- strsplit(ID,"\\.")[[1]][1]
-    } else if (!is.null(ID)) {
-      patientID <- ID
     }
-    ## read sample_info file
-    sampleInfoInput <-  read.table(sampleInfoFile, quote="", 
-                                   header=TRUE, fill=TRUE, 
-                                   sep='', stringsAsFactors=FALSE)
-    ## read ccf file
-    if (!is.null(ccfClusterTsvFile) & !is.null(ccfLociTsvFile)) {
-        ccfClusterInput <- read.table(ccfClusterTsvFile, quote="", 
-                                      header=TRUE, fill=TRUE, 
-                                      sep='\t', stringsAsFactors=FALSE)
-        ccfLociTsvInput <- read.table(ccfLociTsvFile, quote="", 
-                                      header=TRUE, fill=TRUE, 
-                                      sep='\t', stringsAsFactors=FALSE)
+  }
+  
+  ## read sample_info file
+  sampleInfoInput <-  read.table(sampleInfoFile, quote="", 
+                                 header=TRUE, fill=TRUE, 
+                                 sep='', stringsAsFactors=FALSE)
+  ## read ccf file
+  if (!is.null(ccfClusterTsvFile) & !is.null(ccfLociTsvFile)) {
+    ccfClusterInput <- read.table(ccfClusterTsvFile, quote="", 
+                                  header=TRUE, fill=TRUE, 
+                                  sep='\t', stringsAsFactors=FALSE)
+    ccfLociTsvInput <- read.table(ccfLociTsvFile, quote="", 
+                                  header=TRUE, fill=TRUE, 
+                                  sep='\t', stringsAsFactors=FALSE)
+  } else {
+    ccfClusterInput <- NULL
+    ccfLociTsvInput <- NULL
+  }
+  ## Generate patient,lesion and time information
+  mafInput$patient <- ""
+  mafInput$lesion <- ""
+  #mafInput$time <- ""
+  
+  ## combine sample_info_input with maf_input
+  tsbSampleInfo <- unique(sampleInfoInput$sample)
+  tsbLs <- tsbSampleInfo[which(
+    tsbSampleInfo %in% mafInput$Tumor_Sample_Barcode)]
+  for (tsb in tsbLs) {
+    patient <- as.character(
+      sampleInfoInput[which(
+        sampleInfoInput$sample == tsb),]$patient) 
+    lesion <- as.character(
+      sampleInfoInput[which(
+        sampleInfoInput$sample == tsb),]$lesion)
+    #time <- as.character(
+    #    sampleInfoInput[which(
+    #        sampleInfoInput$sample == tsb),]$time)
+    mafInput[which(
+      mafInput$Tumor_Sample_Barcode == tsb),]$patient <- patient
+    mafInput[which(
+      mafInput$Tumor_Sample_Barcode == tsb),]$lesion <- lesion
+    #mafInput[which(
+    #    mafInput$Tumor_Sample_Barcode == tsb),]$time <- time
+  }
+  
+  # ## fix: Error in setattr(x, "row.names", rn)
+  # mafInput$Hugo_Symbol <- as.character(mafInput$Hugo_Symbol)
+  
+  ## filter variant classification
+  if (mutType == "nonSilent"){
+    if (mutNonSilent == "Default"){
+      nonSilent <- c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", 
+                     "Translation_Start_Site", "Nonsense_Mutation", 
+                     "Nonstop_Mutation", "In_Frame_Del",
+                     "In_Frame_Ins", "Missense_Mutation")
     } else {
-        ccfClusterInput <- NULL
-        ccfLociTsvInput <- NULL
+      nonSilent <- mutNonSilent 
     }
-    ## Generate patient,lesion and time information
-    mafInput$patient <- ""
-    mafInput$lesion <- ""
-    #mafInput$time <- ""
-    
-    ## combine sample_info_input with maf_input
-    tsbSampleInfo <- unique(sampleInfoInput$sample)
-    tsbLs <- tsbSampleInfo[which(
-        tsbSampleInfo %in% mafInput$Tumor_Sample_Barcode)]
-    for (tsb in tsbLs) {
-        patient <- as.character(
-            sampleInfoInput[which(
-                sampleInfoInput$sample == tsb),]$patient) 
-        lesion <- as.character(
-            sampleInfoInput[which(
-                sampleInfoInput$sample == tsb),]$lesion)
-        #time <- as.character(
-        #    sampleInfoInput[which(
-        #        sampleInfoInput$sample == tsb),]$time)
-        mafInput[which(
-            mafInput$Tumor_Sample_Barcode == tsb),]$patient <- patient
-        mafInput[which(
-            mafInput$Tumor_Sample_Barcode == tsb),]$lesion <- lesion
-        #mafInput[which(
-        #    mafInput$Tumor_Sample_Barcode == tsb),]$time <- time
-    }
-    
-    # ## fix: Error in setattr(x, "row.names", rn)
-    # mafInput$Hugo_Symbol <- as.character(mafInput$Hugo_Symbol)
-    
-    ## filter variant classification
-    if (mutType == "nonSilent"){
-        if (mutNonSilent == "Default"){
-            nonSilent <- c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", 
-                          "Translation_Start_Site", "Nonsense_Mutation", 
-                          "Nonstop_Mutation", "In_Frame_Del",
-                          "In_Frame_Ins", "Missense_Mutation")
-        } else {
-            nonSilent <- mutNonSilent 
-        }
-        mafInput <- mafInput[which(mafInput$Variant_Classification %in% nonSilent), ]
-    } else if (mutType == "All"){
-        # message("All variant classification submitted")
-    } else {
-        error("mut.type setting error")
-    }
-    
-    ## use.indel filter
-    if(!use.indel){
-        mafInput <- mafInput[which(mafInput$Variant_Type == "SNP"),]
-    }
-    
-    ## filter chromosome
-    if (!is.null(chrSilent)){
-        mafInput <- mafInput[which(!mafInput$Chromosome %in% chrSilent), ]
-    }
-    
-    ## transform data.frame to data.table
-    mafData <- data.table::setDT(mafInput)
-    ccfClusterTsv <- data.table::setDT(ccfClusterInput)
-    ccfLociTsv <- data.table::setDT(ccfLociTsvInput)
-    
-    ## generate classMaf
-    maf <- classMaf(data=mafData, 
-                    ccf.cluster=ccfClusterTsv, 
-                    ccf.loci=ccfLociTsv, 
-                    patientID=patientID, 
-                    ref.build=refBuild)
-    
-    # ## , vafColumn="VAF", 
-    # colnames(maf@data)[colnames(maf@data) == vafColumn] <- "VAF"
-    
-
-    
-    return(maf)
+    mafInput <- mafInput[which(mafInput$Variant_Classification %in% nonSilent), ]
+  } else if (mutType == "All"){
+    # message("All variant classification submitted")
+  } else {
+    error("mut.type setting error")
+  }
+  
+  ## use.indel filter
+  if(!use.indel){
+    mafInput <- mafInput[which(mafInput$Variant_Type == "SNP"),]
+  }
+  
+  ## filter chromosome
+  if (!is.null(chrSilent)){
+    mafInput <- mafInput[which(!mafInput$Chromosome %in% chrSilent), ]
+  }
+  
+  ## transform data.frame to data.table
+  mafData <- data.table::setDT(mafInput)
+  ccfClusterTsv <- data.table::setDT(ccfClusterInput)
+  ccfLociTsv <- data.table::setDT(ccfLociTsvInput)
+  
+  ## generate classMaf
+  maf <- classMaf(data=mafData, 
+                  ccf.cluster=ccfClusterTsv, 
+                  ccf.loci=ccfLociTsv, 
+                  patientID=patientID, 
+                  ref.build=refBuild)
+  
+  # ## , vafColumn="VAF", 
+  # colnames(maf@data)[colnames(maf@data) == vafColumn] <- "VAF"
+  
+  
+  
+  return(maf)
 }
 
 .substrRight <- function(x, n){
-    substr(x, nchar(x)-n+1, nchar(x))
+  substr(x, nchar(x)-n+1, nchar(x))
 }
 
 ## classMaf class
