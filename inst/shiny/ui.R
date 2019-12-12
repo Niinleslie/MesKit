2f6f1e3cf6e969b9537614e7b47f81cfe3e8684a
Ninomoriaty <504430784@qq.com>
2019-12-12 23:14:47 +0800

    hg38

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -12,7 +12,7 @@ suppressMessages(library(shinyBS))
 suppressMessages(library(MesKit))
 
 suppressMessages(library(BSgenome.Hsapiens.UCSC.hg19))
-# suppressMessages(library(BSgenome.Hsapiens.UCSC.hg38))
+suppressMessages(library(BSgenome.Hsapiens.UCSC.hg38))
 
 #sider bar----
 
