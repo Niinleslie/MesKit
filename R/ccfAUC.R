9bbfb72bc1b1f2dce63e448cd7609fbb3b22e0a2
Xin Wang <sylvia.wang@berkeley.edu>
2020-03-06 15:45:01 +0800

    modify scripts

--- R/ccfAUC.R
+++ R/ccfAUC.R
@@ -40,32 +40,21 @@ plotDensity <- function(df){
 
     p <- ggplot2::ggplot(CCF.sort, 
                          aes(x=CCF, y=prop, group=Tumor_Sample_Barcode, color=Tumor_Sample_Barcode)) + 
+      #geom_smooth(na.rm = TRUE, se = FALSE, size = 1.2, formula = y ~ s(x, bs = "cs"), method = "gam") +
       theme_bw() + 
-      geom_smooth(se = FALSE,
-                  method="gam",
-                  formula=y~s(x, bs= "cs"),
-                  size = 1.2,
-                  na.rm = TRUE) +
+      geom_line(size=1.2) +
       xlim(0,1) + ylim(0,1) +
-      coord_fixed() + 
+      coord_fixed() +
       theme(
         #legend.position='none', 
         legend.title = element_blank(),
         title=element_text(size=14, face = "bold"), 
-        text=element_text(size=12, face = "bold"), 
         panel.grid=element_blank(), 
         panel.border=element_blank(), 
-        axis.ticks.length = unit(0.3, "cm"),
-        axis.ticks = element_line(size=1),
-        axis.text = element_text(size=12),
-        axis.title = element_text(size=14),
-        #legend.title=element_text(size=12, face = "bold"),
-        legend.text = element_text(size=12, face = "bold")
-      ) +
-      geom_segment(x = 0, y = -0.05, xend = 1.0, yend = -0.05, size = 1, 
-                   colour = "black", linejoin = "round", lineend = "round") +
-      geom_segment(x = -0.05, y = 0, xend = -0.05, yend = 1.0, size = 1, 
-                   colour = "black", linejoin = "round", lineend = "round") +
+        axis.line=element_line(size=0.7, colour = "black"),
+        axis.text = element_text(size=12, colour = "black"),
+        legend.text = element_text(size=12, face = "bold", colour = "black")
+      )+
       labs(x="CCF", y="Proportion")
     
     return(p)
