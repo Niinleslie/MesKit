34078e461f0a3f3ae30849800d67770f32823e03
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2020-10-01 19:59:52 +0800

    modify descriptions of functions and correct minor errors

--- R/Meskit.R
+++ R/Meskit.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Run the default MesKit app for analysis locally 
 #' 
 #' \code{runMesKit} run MesKit locally 
@@ -11,15 +10,4 @@
 #' @export 
 runMesKit<-function(){ 
   shiny::runApp(system.file("shiny", package = "MesKit"))
-=======
-#' Run the default MesKit app for analysis locally 
-#' 
-#' \code{runMesKit} run MesKit locally 
-#' @author Mengni Liu
-#' 
-#' @return a shiny app window
-#' @export 
-runMesKit<-function(){ 
-  shiny::runApp(system.file("shiny", package = "MesKit"))
->>>>>>> 32b9b431882b8ee2872c4b5b86c6bc237b4f3e12
 } 
